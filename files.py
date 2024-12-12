0e6f6878e7f94af3fe1998e8fde220d1bd2890e9
deciuslee <jlee4subs@gmail.com>
2024-12-12 14:23:54 -0600

    initial commit

---
+++ files.py
@@ -0,0 +1,4 @@
+# Filepath assignments
+housingFile = "C:/Users/deciu/Documents/housing-info.csv"
+incomeFile = "C:/Users/deciu/Documents/income-info.csv"
+zipFile = "C:/Users/deciu/Documents/zip-city-county-state.csv"
